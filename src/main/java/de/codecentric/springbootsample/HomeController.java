b984cc78efec0e0e8ed3de666c41c024e5bb45de
Andreas Walser <andreas.walser.ch@gmail.com>
2017-03-01 08:17:43 +0100

    ...

--- src/main/java/de/codecentric/springbootsample/HomeController.java
+++ src/main/java/de/codecentric/springbootsample/HomeController.java
@@ -24,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
